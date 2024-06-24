2ab2ebe5d3f26c6f1214d04c7946eecdd8509d2c
orjujeng <orjujeng@hotmail.com>
2024-06-25 01:21:15 +0800

    reslove confilct

--- react/reactpoc/src/components/ClassComponents/index.js
+++ react/reactpoc/src/components/ClassComponents/index.js
@@ -2,23 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default class ClassComponents extends React.Component {
-<<<<<<< HEAD
-    state = { flag: true }
-    static propTypes={
-        name:PropTypes.string
-    }
-    render() {
-        const {flag} = this.state
-        // const {key} = 
-        console.log(this)
-        return <span onClick={this.changeFlag} >This is ClassComponents state test {flag?'true':'false'} {this.props.name}</span>
-    }
-
-    changeFlag=()=>{
-        console.log(this)
-        const {flag} = this.state
-        this.setState({flag:!flag})
-=======
     state = { flag: true, 'adv': false }
     static propTypes = {
         name: PropTypes.string
@@ -91,7 +74,6 @@ export default class ClassComponents extends React.Component {
     componentWillUnmount(){
 
         console.log('componentWillUnmount');
->>>>>>> backup
     }
 }
 
